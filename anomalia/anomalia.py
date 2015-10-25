9575cbc3d65b80322298b3eb596dcc62b72b1d07
Sergey Romanov <xxsmotur@gmail.com>
2015-10-25 22:34:27 +0500

    Add basic api for main Anomalia class

--- anomalia/anomalia.py
+++ anomalia/anomalia.py
@@ -9,8 +9,28 @@ class Anomalia:
     def __init__(self, data, datatype, labels=None):
         self.data = data
         self.datatype = datatype
+        self.events = []
+        self.isrunning = False
 
     def preprocessing(self):
         ''' preprocessing data before apply it to algorithms
         '''
         pass
+
+    def addEvent(self, data):
+        if isinstance(data, list):
+            if len(data) == 0:
+                return
+            self.events.append(data)
+
+    def stop(self):
+        ''' Stopping real time anomaly detection
+        '''
+        self.isrunning = False
+
+    def start(self):
+        ''' Start anomaly detection in real time
+        '''
+        self.isrunning = True
+        while self.isrunning:
+            pass
