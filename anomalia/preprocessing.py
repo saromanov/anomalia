2e23b8675e93b45a1d15cfa4770ba602e736f9c1
Sergey Romanov <xxsmotur@gmail.com>
2015-10-27 23:42:33 +0500

    Preprocessing: update

--- anomalia/preprocessing.py
+++ anomalia/preprocessing.py
@@ -2,20 +2,17 @@ from sklearn import preprocessing
 from scipy import ndimage
 
 
-class Preprocessing:
-    def __init__(self, data):
-        self._data = data
+def scale(self):
+    ''' Standardization of data
+    '''
+    return preprocessing.scale(self._data)
 
-    def scale(self):
-        ''' Standardization of data
-        '''
-        return preprocessing.scale(self._data)
+def normalize(self):
+   '''
+      Normalization of data
+   '''
+   return preprocessing.normalize(self._data, norm='l2')
 
-    def normalize(self):
-        ''' Normalization of data
-        '''
-        return preprocessing.normalize(self._data, norm='l2')
-
-    def img_preprocess(self):
-        return ndimage.gaussian_filter(self._data, 5)
+def img_preprocess(self):
+    return ndimage.gaussian_filter(self._data, 5)
 
