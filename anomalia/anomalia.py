b20ca5e0d13d6ab2ff29604f13924edf45fc9588
Sergey Romanov <xxsmotur@gmail.com>
2015-10-27 22:36:36 +0500

    Add file reading

--- anomalia/anomalia.py
+++ anomalia/anomalia.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import numpy as np
 
 logging.basicConfig(level = logging.DEBUG)
 
@@ -10,16 +11,32 @@ class Item:
         self.entity = entity
 
 class Anomalia:
-    def __init__(self, data, datatype, labels=None):
+    def __init__(self, datatype, labels=None, data = None, filepath=None):
         self.data = data
         self.datatype = datatype
         self.events = []
         self.isrunning = False
+        if self.datatype == 'file':
+            self.data = self._read()
+
 
     def _read(self, path):
         if os.path.exists(path) is False:
             logging.error('File {0} is not found'.format(path))
-            return
+            raise Exception("File not found")
+
+        basename = os.basename(path)
+        if basename.find('.csv') != -1:
+            logging.info("Try to load .csv file is loaded")
+            return np.loadtxt(open(path,"rb"),delimiter=",",skiprows=1)
+        else:
+            #load every line
+            f = open(path, 'r')
+            result = []
+            logging.info("Try to load data from file")
+            for line in f.readlines():
+                result.append([float(item) for item in line.split()])
+            return np.array(result)
 
     def preprocessing(self):
         ''' preprocessing data before apply it to algorithms
