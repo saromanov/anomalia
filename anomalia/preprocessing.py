4a9c3be823aff56a281cd7aedc804fb8c4bf0be6
Sergey Romanov <xxsmotur@gmail.com>
2015-11-10 14:07:11 +0500

    Preprocessing now is only fro image or for text data. For datasets, use 'preprocessing' module

--- anomalia/preprocessing.py
+++ anomalia/preprocessing.py
@@ -1,17 +1,5 @@
-from sklearn import preprocessing
 from scipy import ndimage
 
-
-def scale(self):
-    ''' Standardization of data
-    '''
-    return preprocessing.scale(self._data)
-
-def normalize(self):
-   '''
-      Normalization of data
-   '''
-   return preprocessing.normalize(self._data, norm='l2')
 
 def img_preprocess(self):
     return ndimage.gaussian_filter(self._data, 5)
